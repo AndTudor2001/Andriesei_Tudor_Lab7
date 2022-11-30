3f04e5397aa66e0f4dd9cc6149cde9136a1f0bcb
Tudor Andriesei <andrieseitudor@yahoo.com>
2022-11-30 16:32:39 +0200

    Laborator_8

---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,31 @@
+using Andriesei_Tudor_Lab7.Models;
+
+namespace Andriesei_Tudor_Lab7;
+
+public partial class ListEntryPage : ContentPage
+{
+	public ListEntryPage()
+	{
+		InitializeComponent();
+	}
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		listView.ItemsSource=await App.Database.GetShopListsAsync();
+	}
+	async void OnShopListAddedClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new ListPage
+		{ BindingContext = new ShopList()});
+	}
+	async void OnListViewItemsSelected(object sender, SelectedItemChangedEventArgs e)
+	{
+		if(e.SelectedItem!=null)
+		{
+			await Navigation.PushAsync(new ListEntryPage
+			{
+				BindingContext = e.SelectedItem as ShopList
+			});
+		}
+	}
+}
