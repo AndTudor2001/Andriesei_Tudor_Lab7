d8008cf2f61665119dad0d73dbdbb394174243a1
Tudor Andriesei <andrieseitudor@yahoo.com>
2022-12-14 19:36:15 +0200

    Laborator9

--- ListEntryPage.xaml.cs
+++ ListEntryPage.xaml.cs
@@ -4,28 +4,30 @@ namespace Andriesei_Tudor_Lab7;
 
 public partial class ListEntryPage : ContentPage
 {
-	public ListEntryPage()
-	{
-		InitializeComponent();
-	}
-	protected override async void OnAppearing()
-	{
-		base.OnAppearing();
-		listView.ItemsSource=await App.Database.GetShopListsAsync();
-	}
-	async void OnShopListAddedClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new ListPage
-		{ BindingContext = new ShopList()});
-	}
-	async void OnListViewItemsSelected(object sender, SelectedItemChangedEventArgs e)
-	{
-		if(e.SelectedItem!=null)
-		{
-			await Navigation.PushAsync(new ListEntryPage
-			{
-				BindingContext = e.SelectedItem as ShopList
-			});
-		}
-	}
+    public ListEntryPage()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
 }
